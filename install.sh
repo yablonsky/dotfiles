2a662d36476a545a1db917ba0979ecf376728ddd
Andriy Yablonskyy <andriy.yablonskyy@gmail.com>
2017-07-15 16:43:56 -0400

    small install improvement

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #! /bin/bash
 
 DOTFILES=$(pwd)
-ln -s "$DOTFILES/shell/.bashrc" "$HOME/.bashrc"
-ln -s "$DOTFILES/shell/.inputrc" "$HOME/.inputrc"
-ln -s "$DOTFILES/shell/.editrc" "$HOME/.editrc"
+ln -sf "$DOTFILES/shell/.bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES/shell/.inputrc" "$HOME/.inputrc"
+ln -sf "$DOTFILES/shell/.editrc" "$HOME/.editrc"
 
-ln -s "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
 if [ ! -d "$HOME/.tmux/plugins" ]; then
     mkdir -p "$HOME/.tmux/plugins"
     git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
     echo "Don't for forget to install tmux plugins (prefix + I)"
 fi
 
-ln -s "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
 if [ ! -d "$HOME/.vim" ]; then
-    mkdir "$HOME/.vim"
+    mkdir -p "$HOME/.vim/bundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
     ln -s "$DOTFILES/vim/vundles.vim" "$HOME/.vim/vundles.vim"
     ln -s "$DOTFILES/vim/settings.vim" "$HOME/.vim/settings.vim"
     ln -s -T "$DOTFILES/vim/settings" "$HOME/.vim/settings"
+    vim +PluginInstall +qall
 fi
 
-ln -s "$DOTFILES/X/.xinitrc" "$HOME/.xinitrc"
-ln -s "$DOTFILES/X/.Xresources" "$HOME/.Xresources"
+ln -sf "$DOTFILES/X/.xinitrc" "$HOME/.xinitrc"
+ln -sf "$DOTFILES/X/.Xresources" "$HOME/.Xresources"
