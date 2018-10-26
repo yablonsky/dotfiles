2c84b0fba46ad251998889faa352624819ef2acb
Andriy Yablonskyy <andriy.yablonskyy@gmail.com>
2018-10-26 11:46:12 -0400

    gtk wal configs

--- install.sh
+++ install.sh
@@ -7,41 +7,45 @@ ln -sf "$DOTFILES/shell/.inputrc" "$HOME/.inputrc"
 ln -sf "$DOTFILES/shell/.editrc" "$HOME/.editrc"
 ln -sf "$DOTFILES/.gitconfig" "$HOME/.gitconfig"
 
-echo "Installing Bash-Powerline"
+echo "Linking Bash-Powerline"
 ln -sf "$DOTFILES/shell/.bash-powerline.sh" "$HOME/.bash-powerline.sh"
 
-echo "Installing i3 config"
+echo "Linking i3 config"
 ln -sf -T "$DOTFILES/config/i3" "$HOME/.config/i3"
-echo "Installing i3status config"
+echo "Linking i3status config"
 ln -sf -T "$DOTFILES/config/i3status" "$HOME/.config/i3status"
 
-echo "Installing rofi config"
+echo "Linking rofi config"
 ln -sf -T "$DOTFILES/config/rofi" "$HOME/.config/rofi"
 
-echo "Installing alacritty config"
+echo "Linking alacritty config"
 ln -sf -T "$DOTFILES/config/alacritty" "$HOME/.config/alacritty"
 
 ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
 if [ ! -d "$HOME/.tmux/plugins" ]; then
-    echo "Installing Tmux plugins"
+    echo "Linking Tmux plugins"
     mkdir -p "$HOME/.tmux/plugins"
     git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
     echo "Don't for forget to install tmux plugins (prefix + I)"
 fi
 
 if [ ! -d "$HOME/.config/nvim" ]; then
-    echo "Installing NVIM config"
+    echo "Linking NVIM config"
     ln -s -T "$DOTFILES/config/nvim" "$HOME/.config/nvim"
     mkdir -p "$HOME/.config/nvim/bundle"
     git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.config/nvim/bundle/Vundle.vim"
     nvim +PluginInstall +qa
 fi
 
-echo "Installing pywal config"
+echo "Linking pywal config"
 ln -sf "$HOME/.cache/wal/colors.Xresources" "$HOME/.Xresources"
 
-echo "Installing compton config"
+echo "Linking compton config"
 ln -sf "$DOTFILES/config/compton.conf" "$HOME/.config/compton.conf"
 
-echo "Installing redshift config"
+echo "Linking redshift config"
 ln -sf -T "$DOTFILES/config/redshift" "$HOME/.config/redshift"
+
+echo "Linking gtk configs"
+ln -sf "$DOTFILES/config/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
+ln -sf "$DOTFILES/config/gtk-3.0/settings.ini" "$HOME/.config/gtk-3.0/settings.ini"
