8119cb97f87f0f312299322ac20da0645060fb88
Andriy Yablonskyy <andriy.yablonskyy@gmail.com>
2019-05-07 10:26:01 -0400

    better sys menu

--- rofi_scripts/system_power.sh
+++ rofi_scripts/system_power.sh
@@ -5,7 +5,7 @@ widthpercent=30
 
 typeset -A menu
 menu=(
-    [S Shutdown]="systemctl poweroff -i"
+    [q Shutdown]="systemctl poweroff -i"
     [r Reboot]="systemctl reboot"
     [l Lock]="i3lock -c 000000 && sleep 1"
     [s Suspend]="i3lock -c 000000 && sleep 1 && systemctl suspend"
