30d9d833143502765999c83983a86b66b4adb7b6
Andriy Yablonskyy <andriy.yablonskyy@gmail.com>
2019-05-15 14:22:00 -0400

    light lock on suspend

--- rofi_scripts/system_power.sh
+++ rofi_scripts/system_power.sh
@@ -8,7 +8,7 @@ menu=(
     [q Shutdown]="systemctl poweroff -i"
     [r Reboot]="systemctl reboot"
     [l Lock]="i3lock -c 000000 && sleep 1"
-    [s Suspend]="i3lock -c 000000 && sleep 1 && systemctl suspend"
+    [s Suspend]="systemctl suspend"
     [h Hibernate]="i3lock -c 000000 && sleep 1 && systemctl hibernate"
     [e Logout]="i3-msg exit"
 )
